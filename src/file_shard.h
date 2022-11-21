26d7fd65b7acdccadf339311a5944e7069e4d59c
Jithin-K-Sojan <jithinsojan99@gmail.com>
2022-11-21 12:14:11 -0500

    Addding mr_spec and file_shard

--- src/file_shard.h
+++ src/file_shard.h
@@ -3,15 +3,106 @@
 #include <vector>
 #include "mapreduce_spec.h"
 
+//ADDED
+#include <fstream>
+
+
+// Created an additioinal structure for ease of moving complete shards around.
+struct MiniShard{
+     std::string fileName;
+     long int start;
+     long int end;
+};
 
 /* CS6210_TASK: Create your own data structure here, where you can hold information about file splits,
      that your master would use for its own bookkeeping and to convey the tasks to the workers for mapping */
 struct FileShard {
-
+     std::vector<MiniShard> miniShards; 
 };
 
 
 /* CS6210_TASK: Create fileshards from the list of input files, map_kilobytes etc. using mr_spec you populated  */ 
 inline bool shard_files(const MapReduceSpec& mr_spec, std::vector<FileShard>& fileShards) {
+
+     long int map_kbs = mr_spec.map_kilobytes;
+
+     for (auto inputFile: mr_spec.input_files){
+
+          // if (currShardSize==0){
+          //      struct FileShard newFileShard;
+          //      fileShards.push_back(newFileShard);
+          // }
+
+          std::ifstream inputFileStream(inputFile);
+
+          inputFileStream.seekg(0,std::ifstream::end);
+          long int inputFileSize = inputFileStream.tellg();
+
+          // std::cout<<inputFile<<" size: "<<inputFileSize<<std::endl;
+
+          inputFileStream.seekg(0);
+
+          long int currShardSize = 0;
+          long int pos = 0;
+          
+          while (pos<inputFileSize){
+               if (currShardSize==0){
+                    
+                    FileShard newFileShard;
+                    fileShards.push_back(newFileShard);
+               }
+
+
+               struct MiniShard newMiniShard;
+               if ((inputFileSize-pos)>(map_kbs*1024)){
+                    inputFileStream.seekg((map_kbs*1024),std::ifstream::cur);
+                    std::string temp;
+                    // Should we peek and check if it is at '\n'? Or does getline return empty in that case?
+
+                    if (getline(inputFileStream, temp)){
+
+                         newMiniShard.fileName = inputFile;
+                         newMiniShard.start = pos;
+                         newMiniShard.end = inputFileStream.tellg();
+
+                    }
+                    else{
+                         ; // This case is probably not possible.
+
+
+                    }
+               }
+               else{
+                    // The file has to end on a '\n' right?
+                    newMiniShard.fileName = inputFile;
+                    newMiniShard.start = pos;
+                    newMiniShard.end = inputFileSize;
+               }
+
+
+               std::cout<<inputFileStream.tellg()<<std::endl;
+
+               pos = inputFileStream.tellg();
+
+               currShardSize += (newMiniShard.end - newMiniShard.start);
+               fileShards[fileShards.size()-1].miniShards.push_back(newMiniShard);
+
+               if (currShardSize>(map_kbs*1024)){
+                    currShardSize = 0;
+               }
+
+          }
+
+          inputFileStream.close();
+
+     }
+
+     for (auto i: fileShards){
+          std::cout<<"New Shard\n";
+          for (auto j: i.miniShards){
+               std::cout<<j.fileName<<" "<<j.start<<" "<<j.end<<std::endl;
+          }
+     }
+
 	return true;
 }
--- src/mapreduce_spec.h
+++ src/mapreduce_spec.h
@@ -2,20 +2,146 @@
 
 #include <string>
 
+//ADDED
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+
 
 /* CS6210_TASK: Create your data structure here for storing spec from the config file */
 struct MapReduceSpec {
-
+	// Don't know if we need any more fields, just took whatever is present in config.ini for now.
+	int n_workers;
+	std::vector<std::string> worker_ipaddr_ports;
+	std::vector<std::string> input_files;
+	std::string output_dir;
+	int n_output_files;
+	long int map_kilobytes;
+	std::string user_id;
 };
 
+// ADDED
+inline std::string trim_string(std::string str){
+	int n = str.size();
+
+	int i;
+	for (i = 0;i<str.size();i++){
+		if (str[i]!=' ') break;
+	}
+	std::string ltrimmed = str.substr(i);
+
+	while(ltrimmed[ltrimmed.size()-1]==' '){
+		ltrimmed.pop_back();
+	}
+
+	return ltrimmed;
+}
 
 /* CS6210_TASK: Populate MapReduceSpec data structure with the specification from the config file */
 inline bool read_mr_spec_from_config_file(const std::string& config_filename, MapReduceSpec& mr_spec) {
+
+	// Using ifstream: https://cplusplus.com/doc/tutorial/files/
+	std::string line;
+	std::ifstream configFile(config_filename);
+
+	if (configFile.is_open()){
+		while (getline(configFile,line)){
+			
+			// Splitting the line based on the key and value.
+			std::stringstream kvStream(line);
+			std::string Key,Value;
+
+			if (getline(kvStream, Key, '=')){
+				
+				std::string key = trim_string(Key);
+
+				if (!getline(kvStream, Value, '=')){
+					
+					std::cout<<"No value present for key "<<key<<" in config file.\n";
+					configFile.close();
+					return false;
+				}
+
+				std::string value = trim_string(Value);
+
+				if (key=="n_workers"){
+					mr_spec.n_workers = stoi(value);
+					// std::cout<<mr_spec.n_workers<<std::endl;
+				}
+				else if (key=="worker_ipaddr_ports"){
+					std::stringstream addressStream(value);
+					std::string address;
+
+					while (getline(addressStream, address, ',')){
+						mr_spec.worker_ipaddr_ports.push_back(address);
+					}
+
+					// std::cout<<mr_spec.worker_ipaddr_ports[0]<<std::endl;
+				}
+				else if (key=="input_files"){
+					std::stringstream inputFileStream(value);
+					std::string inputFile;
+
+					while (getline(inputFileStream, inputFile, ',')){
+						mr_spec.input_files.push_back(inputFile);
+					}
+
+					// std::cout<<mr_spec.input_files[0]<<std::endl;
+				}
+				else if (key=="output_dir"){
+					mr_spec.output_dir = value;
+					// std::cout<<mr_spec.output_dir<<std::endl;
+				}
+				else if (key=="n_output_files"){
+					mr_spec.n_output_files = stoi(value);
+					// std::cout<<mr_spec.n_output_files<<std::endl;
+				}
+				else if (key=="map_kilobytes"){
+					mr_spec.map_kilobytes = stol(value);
+					// std::cout<<mr_spec.map_kilobytes<<std::endl;
+				}
+				else if (key=="user_id"){
+					mr_spec.user_id = value;
+					// std::cout<<mr_spec.user_id<<std::endl;
+				}
+				else{
+					std::cout<<"Undefined key "<<key<<" set in config file "<<config_filename<<" \n";
+					configFile.close();
+					return false;
+				}
+
+			}
+
+		}
+	}
+	configFile.close();
+
 	return true;
 }
 
 
 /* CS6210_TASK: validate the specification read from the config file */
 inline bool validate_mr_spec(const MapReduceSpec& mr_spec) {
+
+	// The output directory would be created later I believe.
+
+	if (mr_spec.n_workers > mr_spec.worker_ipaddr_ports.size()){
+		std::cout<<"Number of specified worker addresses less than n_workers in the configuration!\n";
+		return false;
+	}
+
+	for (auto fileName: mr_spec.input_files){
+		std::ifstream inputFile(fileName);
+		if(!inputFile.good()){
+			std::cout<<"Input file "<<fileName<<" not found!\n";
+			inputFile.close();
+			return false;
+		}
+		inputFile.close();
+	}
+
+	std::cout<<"Looks good!\n";
+
 	return true;
 }
