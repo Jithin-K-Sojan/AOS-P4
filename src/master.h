4a36d09c9f844c9b00e0d4839df32f0bbf398f45
Kuilin Li <kuilin@gmail.com>
2022-12-02 21:56:34 -0500

    done? gradescope 1 https://www.gradescope.com/courses/424155/assignments/2182758/submissions/152155960

--- src/master.h
+++ src/master.h
@@ -4,6 +4,7 @@
 #include "file_shard.h"
 
 #include <grpc++/grpc++.h>
+#include <filesystem> 
 #include "masterworker.grpc.pb.h"
 
 using grpc::ClientAsyncResponseReader;
@@ -25,55 +26,106 @@ class Master {
 
 	private:
 		/* NOW you can add below, data members and member functions as per the need of your implementation*/
+    bool run_ran = false; // just in case
+
     const MapReduceSpec _mr_spec;
 
-    // for tracking FileShard state in master
-    struct FileShardData {
-      const struct FileShard shard;
-      bool done = false;
+    size_t _tasks_idx = 0; // for scheduler, which next one to do
 
-      bool ctx_valid = false; // true if ctx should be cancelled on cancel
-      grpc::ClientContext ctx;
-      masterworker::JobReply reply;
-      grpc::Status status;
+    // map tasks, one per FileShard
+    struct MapTask {
+      const FileShard shard;
 
-      FileShardData(const FileShard &s) : shard(s) {}
+      bool done;
+      masterworker::JobReply *reply;
+
+      void fill_request(masterworker::JobRequest& req) {
+        req.set_map_reduce(0);
+        req.set_job_id("map_" + std::to_string((size_t)this));
+
+        for (auto ms : shard.miniShards) {
+          masterworker::FileArgs* fa = req.add_input_files();
+          fa->set_file_path(ms.fileName);
+          fa->set_start_offset(ms.start);
+          fa->set_end_offset(ms.end);
+        }
+      };
+
+      MapTask(const FileShard& s) : shard(s), done(false), reply(NULL) {};
     };
-    std::vector<struct FileShardData> _fs;
-    size_t _fs_idx = 0;
+    std::vector<MapTask> _map_tasks;
 
-    // state machine for metadata for a single worker, must be malloc'd/new'd
-    // passes own tag into grpc callback for casting later back to WorkerData*
-    // re-instantiated for reduce after map completes
+    // reducer tasks, there are R of them
+    struct ReduceTask {
+      size_t partition;
+      const std::vector<MapTask>& map_tasks;
+      std::string output_dir;
+
+      bool done;
+      masterworker::JobReply *reply;
+
+      void fill_request(masterworker::JobRequest& req) {
+        req.set_map_reduce(1);
+        req.set_job_id("reduce_" + std::to_string((size_t)this));
+
+        for (auto &x : map_tasks) {
+          assert(partition < x.reply->output_files_size());
+          auto& f = x.reply->output_files(partition);
+
+          masterworker::FileArgs* fa = req.add_input_files();
+          fa->set_file_path(f.file_path());
+          fa->set_start_offset(f.start_offset());
+          fa->set_end_offset(f.end_offset());
+        }
+        req.set_output_dir(output_dir);
+      };
+
+      ReduceTask(const size_t& p, const std::vector<MapTask>& m, std::string o) : partition(p), map_tasks(m), output_dir(o), done(false), reply(NULL) {};
+    };
+    std::vector<ReduceTask> _reduce_tasks;
+
+    // metadata for a single worker
+    template<typename Task>
     class WorkerData {
+      static_assert(std::is_same<Task, MapTask>::value || std::is_same<Task, ReduceTask>::value, "Task must be MapTask or ReduceTask");
       private:
+        // current job's task slot
+        // we only write to this if _task.done is false, and rpc succeeds
+        Task *_task;
+
         const MapReduceSpec& _mr_spec;
-        std::vector<FileShardData>& _fs;
-        size_t &_fs_idx;
+        std::vector<Task>& _tasks;
+        size_t& _tasks_idx;
         const std::string _ip;
-        const bool _map_reduce; // whether it's map or reduce
 
         CompletionQueue &_cq;
         std::shared_ptr<grpc::Channel> _ch;
 
+        grpc::ClientContext *ctx = NULL;
+        masterworker::JobReply *reply = NULL;
+        grpc::Status status;
+
       public:
-        WorkerData(const MapReduceSpec& mr_spec, std::vector<FileShardData>& fs, size_t &fs_idx, std::string ip, bool map_reduce, CompletionQueue &cq) :
-          _mr_spec(mr_spec), _fs(fs), _ip(ip), _fs_idx(fs_idx), _map_reduce(map_reduce), _cq(cq) { };
+        WorkerData(const MapReduceSpec& mr_spec, std::vector<Task>& fsds, size_t &fsds_idx, std::string ip, CompletionQueue &cq) :
+          _mr_spec(mr_spec), _tasks(fsds), _tasks_idx(fsds_idx), _ip(ip), _cq(cq) { begin(); };
 
         void begin() {
-          // get next one to do
-          // _fs_idx is the next one, except if it's done already
+          // printf("%p begin\n", this);
+          // determine which one to do
+          // _tasks_idx is the next one, except if it's done already
           // it's okay to have all the workers working on the same last one, i think, at the way end
           // since they all can then just be cancelled when the first one completes
           {
-            size_t initial_idx = _fs_idx;
-            while (_fs[_fs_idx].done) {
-              _fs_idx = (_fs_idx+1)%_fs.size();
-              if (_fs_idx == initial_idx) {
-                // should not loop all the way around, since then begin() should never have been called (all done)
-                assert(false);
+            size_t initial_idx = _tasks_idx;
+            while (_tasks[_tasks_idx].done) {
+              _tasks_idx = (_tasks_idx+1)%_tasks.size();
+              if (_tasks_idx == initial_idx) {
+                // everything is done, nothing else to do (this drops out of the cq loop)
+                return;
               }
             }
+            _task = &_tasks[_tasks_idx];
+            _tasks_idx = (_tasks_idx+1)%_tasks.size();
           }
 
           // connect to server
@@ -82,30 +134,55 @@ class Master {
 
           // request job
           masterworker::JobRequest req;
-          req.set_job_id("job");
           req.set_user_id(_mr_spec.user_id);
-          req.set_map_reduce(_map_reduce);
           req.set_num_partitions(_mr_spec.n_output_files);
-          for (auto ms : _fs[_fs_idx].shard.miniShards) {
-            masterworker::FileArgs* fa = req.add_input_files();
-            fa->set_file_path(ms.fileName);
-            fa->set_start_offset(ms.start);
-            fa->set_end_offset(ms.end);
-          }
-          req.set_output_dir("output_dir");
+          _task->fill_request(req);
+
+          // dynamically allocate reply
+          reply = new masterworker::JobReply();
+
+          // set deadline and make context
+          ctx = new grpc::ClientContext();
+          ctx->set_deadline(std::chrono::system_clock::now() + std::chrono::milliseconds(1000));
 
           std::unique_ptr<ClientAsyncResponseReader<masterworker::JobReply>> rpc(
-            stub->AsyncrunJob(&_fs[_fs_idx].ctx, req, _cq));
+            stub->AsyncrunJob(ctx, req, &_cq));
 
-          rpc->Finish(&_fs[_fs_idx].reply, &_fs[_fs_idx].status, this);
+          rpc->Finish(reply, &status, this);
         }
 
         void done() {
-          
+          // printf("%p done\n", this);
+
+          // check task done or worker reported failure
+          if (_task->done || reply->job_status() == 0) {
+            fail();
+            return;
+          }
+
+          // dispose of ctx
+          delete ctx;
+          ctx = NULL;
+
+          // set and pickup new task
+          _task->reply = reply;
+          _task->done = true;
+          begin();
         }
 
         void fail() {
-          // wip
+          // printf("%p fail\n", this);
+          delete reply;
+          reply = NULL;
+          delete ctx;
+          ctx = NULL;
+          begin();
+        }
+
+        void cancel() {
+          // printf("%p cancel\n", this);
+          if (ctx) ctx->TryCancel();
+          // small memory leak here, but this is ok
         }
     };
 };
@@ -114,31 +191,62 @@ class Master {
 /* CS6210_TASK: This is all the information your master will get from the framework.
 	You can populate your other class data members here if you want */
 Master::Master(const MapReduceSpec& mr_spec, const std::vector<FileShard>& file_shards) : _mr_spec(mr_spec) {
-  _fs.reserve(file_shards.size());
-  for (auto s : file_shards) _fs.emplace_back(s);
+  _map_tasks.reserve(file_shards.size());
+  for (auto s : file_shards) _map_tasks.emplace_back(s);
 }
 
 /* CS6210_TASK: Here you go. once this function is called you will complete whole map reduce task and return true if succeeded */
 bool Master::run() {
+  assert(!run_ran);
+  run_ran = true;
+
   // single cq because master has single thread, we want to wait for it outside WorkerData
-  CompletionQueue cq;
+  CompletionQueue map_cq, reduce_cq;
 
-  // connect to all workers
-  for (std::string ip : _mr_spec.worker_ipaddr_ports) new WorkerData(_mr_spec, _fs, &_fs_idx, ip, cq);
+  // connect to all workers for map task
+  std::vector<WorkerData<MapTask>*> map_workers;
+  map_workers.reserve(_mr_spec.worker_ipaddr_ports.size());
+  for (std::string ip : _mr_spec.worker_ipaddr_ports) map_workers.push_back(new WorkerData<MapTask>(_mr_spec, _map_tasks, _tasks_idx, ip, map_cq));
 
-  // register fail handler
-  // wip AsyncNotifyWhenDone to fail afer casting tag
-
-  // receive loop
   void* tag;
   bool ok;
-  while (!std::all_of(_fs.begin(), _fs.end(), [](auto &s){ return s.done; })) {
-    if (!(cq.Next(&tag, &ok))) {
-      // queue shutting down
+
+  // process workers until map task done
+  while (!std::all_of(_map_tasks.begin(), _map_tasks.end(), [](auto &s){ return s.done; })) {
+    if (!(map_cq.Next(&tag, &ok))) {
+      // queue shutting down for some reason
+      printf("queue shutdown?\n");
       return false;
     }
-    if (!ok) ((WorkerData*)tag)->fail();
-    else ((WorkerData*)tag)->step();
+    if (!ok) ((WorkerData<MapTask>*)tag)->fail();
+    else ((WorkerData<MapTask>*)tag)->done();
+  }
+
+  // make directory for output
+  assert(std::filesystem::create_directory(_mr_spec.output_dir.c_str()));
+
+  // cancel all workers for reduce (all but one will be busy on the last one)
+  for (auto w : map_workers) w->cancel();
+
+  // set up reduce tasks
+  _reduce_tasks.reserve(_mr_spec.n_output_files);
+  for (size_t i=0; i<_mr_spec.n_output_files; i++) _reduce_tasks.emplace_back(i, _map_tasks, _mr_spec.output_dir);
+
+  // begin reduce task (note: we cannot destruct any workers since old callbacks might still be in cq... so just use a new cq)
+  _tasks_idx = 0;
+  std::vector<WorkerData<ReduceTask>*> reduce_workers;
+  reduce_workers.reserve(_mr_spec.worker_ipaddr_ports.size());
+  for (std::string ip : _mr_spec.worker_ipaddr_ports) reduce_workers.push_back(new WorkerData<ReduceTask>(_mr_spec, _reduce_tasks, _tasks_idx, ip, reduce_cq));
+
+  // process workers until reduce task done
+  while (!std::all_of(_reduce_tasks.begin(), _reduce_tasks.end(), [](auto &s){ return s.done; })) {
+    if (!(reduce_cq.Next(&tag, &ok))) {
+      // queue shutting down for some reason
+      printf("queue shutdown?\n");
+      return false;
+    }
+    if (!ok) ((WorkerData<ReduceTask>*)tag)->fail();
+    else ((WorkerData<ReduceTask>*)tag)->done();
   }
 
 	return true;
